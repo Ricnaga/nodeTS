3579a9667501b26c0111c54a137a8045f0521734
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-11-25 17:15:20 -0300

    all test passed

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -38,7 +38,7 @@ describe('CreateUser', () => {
                 password:'123456'
             })
 
-            expect(
+            await expect(
                 createUser.execute({
                 name: "Jack the ripper",
                 email: "jack@theripper.com",
