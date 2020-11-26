b77072ae5236609488a1fa1cd12b76907e600591
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-11-26 12:02:29 -0300

    reset password test done

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -12,12 +12,20 @@ class FakeUserTokensRepository implements IUserTokensRepository{
                 id: uuid(),
                 token: uuid(),
                 user_id,
+                create_at: new Date(),
+                update_at: new Date()
             })
 
             this.userTokens.push(userToken)
 
             return userToken
          }
+
+         public async findByToken(token: string): Promise<UserToken | undefined>{
+             const userToken = this.userTokens.find(findToken => findToken.token == token)
+
+             return userToken
+            }
 }
 
 export default FakeUserTokensRepository;
