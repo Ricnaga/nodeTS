6df79d411cd11a1d6203a4acebc5195d66105808
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-11-28 15:06:40 -0300

    list available hours done

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,7 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO'
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO'
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO'
 
 export default interface IAppointmentsRepository{
     create(data: ICreateAppointmentDTO): Promise<Appointment>;
@@ -8,4 +9,8 @@ export default interface IAppointmentsRepository{
     findAllInMonthFromProvider(
         data: IFindAllInMonthFromProviderDTO
         ): Promise<Appointment []>;
+    findAllInDayFromProvider(
+        data: IFindAllInDayFromProviderDTO
+        ): Promise<Appointment []>;
+        
 }
