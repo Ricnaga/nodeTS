ac791e264b40a02366625bdc29312f119bcae012
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-11-27 21:40:59 -0300

    filterng appointment in month done

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,8 +1,9 @@
-import { getRepository, Repository} from 'typeorm';
+import { getRepository, Repository, Raw} from 'typeorm';
 
 import Appointment from '../entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO'
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO'
 
 class AppointmentsRepository implements IAppointmentsRepository{
     private ormRepository: Repository<Appointment>;
@@ -16,6 +17,26 @@ class AppointmentsRepository implements IAppointmentsRepository{
             where: { date },
         })
             return findAppointment
+    }
+
+    public async findAllInMonthFromProvider({
+        provider_id,
+        month,
+        year
+    }:IFindAllInMonthFromProviderDTO): Promise<Appointment[]>{
+        const parsedMonth = String(month).padStart(2, '0')
+        
+        const appointments = await this.ormRepository.find({
+            where:{
+                provider_id,
+                date: Raw(
+                    dateFieldName =>
+                    `to_char(${dateFieldName}, 'MM-YYYY') = ${parsedMonth}-${year}`
+                    ),
+            }
+        })
+
+        return appointments
     }
 
     public async create({provider_id , date}: ICreateAppointmentDTO): Promise<Appointment>{
