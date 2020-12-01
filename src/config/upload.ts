7859439b574512784734863989fd5b1758fde982
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-12-01 12:22:19 -0300

    upload avatar to amazons3 done

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,48 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, {StorageEngine} from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..','tmp')
 
+interface IUploadConfig{
+    driver: 's3' | 'disk';
+    
+    tmpFolder: string;
+    uploadsFolder: string;
+
+    multer:{
+        storage:StorageEngine
+    };
+
+    config:{
+        disk:{};
+        aws:{
+            bucket:string;
+        }
+    };
+}
+
 export default {
+    driver: process.env.STORAGE_DRIVER,
     tmpFolder,
     uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-    storage: multer.diskStorage({
-        destination: tmpFolder,
-        filename(request, file, callback){
-            const filehash = crypto.randomBytes(10).toString('hex')
-            const fileName = `${filehash}-${file.originalname}`
-            
-            return callback(null, fileName)
+    multer:{
+        storage: multer.diskStorage({
+            destination: tmpFolder,
+            filename(request, file, callback){
+                const filehash = crypto.randomBytes(10).toString('hex')
+                const fileName = `${filehash}-${file.originalname}`
+                
+                return callback(null, fileName)
+            },
+        }),
+    },
+
+    config:{
+        disk: {},
+        aws:{
+            bucket:'app-gobarber-2'
         }
-    }),
-}
+    }
+} as IUploadConfig;
