c4b5ffa0453e60bfe288774237025dfd6e72dfb3
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-12-01 16:02:42 -0300

    invalidate cache done

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,8 +1,9 @@
 import {injectable, inject} from 'tsyringe'
 
-import User from '@modules/users/infra/typeorm/entities/User'
-import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
+import IUsersRepository from '@modules/users/repositories/IUsersRepository'
+
+import User from '@modules/users/infra/typeorm/entities/User'
 
 interface IRequest{
     user_id:string;
@@ -15,20 +16,25 @@ class ListProviderService{
         private usersRepository:IUsersRepository,
 
         @inject('CacheProvider')
-        private cacheProvider: ICacheProvider
+        private cacheProvider: ICacheProvider,
         ){}
 
     public async execute({
         user_id, 
     }: IRequest): Promise<User[]> {     
-        let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`)
+        let users = await this.cacheProvider.recover<User[]>(
+            `providers-list:${user_id}`
+            )
 
         if(!users){
             users = await this.usersRepository.findAllProviders({
                 except_user_id: user_id
                 })
-                
-                await this.cacheProvider.save(`providers-list:${user_id}`, users)
+
+                await this.cacheProvider.save(
+                    `providers-list:${user_id}`, 
+                    users
+                    )
         }
 
         return users
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,9 +1,9 @@
 import {injectable, inject} from 'tsyringe'
 
 import AppError from '@shared/errors/AppError';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
 import IUsersRepository from '../repositories/IUsersRepository'
 import IHashProvider from '../providers/HashProvider/models/IHashProvider'
-import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
 
 import User from '../infra/typeorm/entities/User';
 
@@ -41,6 +41,8 @@ class CreateUserService{
             email,
             password: hashedPassword,
         })
+
+        await this.cacheProvider.invalidatePrefix('providers-list:*')
 
         return user
     }
